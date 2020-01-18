7863fd164a61303ed68c39e47502ceb9b0fad198
Norrico Gerald Biason <geraldbiasonjob31@gmail.com>
2020-01-18 12:43:57 +0800

    some changes

--- http.js
+++ http.js
@@ -11,13 +11,20 @@ const server = http.createServer((req, res) => {
     if(req.url === '/') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
+            res.end(data);
         });
     }
     if(req.url === '/user') {
         res.write('Welcome user levi')
         res.end();
+    }
+    if(req.url === '/api/users') {
+        const users = [
+            { name: "levi", age: 20 },
+            { name: "mikasa", age: 18 },
+            { name: "gerald", age: 26 }
+        ]
+        res.end(JSON.stringify(users));
     }
 });
 server.listen(3000, () => console.log('Server is up and running'));
